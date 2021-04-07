28becdde95e884864688975cf0112e65ed31a14d
Hugo Araujo <araujohugo@msn.com>
2021-04-07 09:41:33 +0200

    Affichage de la facture complète

--- app/Http/Controllers/facture.php
+++ app/Http/Controllers/facture.php
@@ -40,12 +40,19 @@ class facture extends Controller
         // } else {
         //     $DateEcheance = date('Y-m-t');
         // }
-
+        $j = 0;
         $adresse = DB::select('SELECT NomSport, Nom, Addrs, Ville, CodPost, Sport FROM LIGUE, Facture WHERE LIGUE.NumLigue = Facture.NumLigue AND idFacture = ' . $_GET['idFacture'] . '');
         $client = DB::select('SELECT idFacture, NumLigue, DateDeb, DateEcheance FROM Facture WHERE idFacture = ' . $_GET['idFacture'] . '');
-        $contenu = DB::select('SELECT ContenuFacture.NomType, NomMat, Qte, Prix FROM ContenuFacture, Facture, Prestations WHERE
+        $contenu = DB::select('SELECT ContenuFacture.NomType as "ContenuFacture", NomMat, Qte, Prix FROM ContenuFacture, Facture, Prestations WHERE
         ContenuFacture.idFacture = Facture.idFacture AND ContenuFacture.NomType = Prestations.NomType AND Facture.idFacture = ' . $_GET['idFacture'] . '');
-
-        return view('facture', compact('adresse','client','contenu'));
+        foreach ($contenu as $contenudata) {
+            $prix[$j] = ($contenudata -> Qte) * ($contenudata -> Prix);
+            $j++;
+        }
+        $prix_total = 0;
+        for ($i=0; $i < $j; $i++) {
+            $prix_total = $prix_total + $prix[$i];
+        }
+        return view('facture', compact('adresse','client','contenu', 'prix', 'prix_total'));
     }
 }
--- resources/views/facture.blade.php
+++ resources/views/facture.blade.php
@@ -1,3 +1,8 @@
+<style>
+    .size {
+        font-size: 10pt;
+    }
+</style>
 @extends('layout')
 @section('content')
 <div class="mx-auto" style="width: 800px;">
@@ -6,9 +11,9 @@
             <td>
                 <img src="mdl.png" height="150px" width="150px">
                 <center>
-                    <strong>
-                        <h1> Facture </h1>
-                    </strong>
+                    <h1>
+                        <strong> Facture </strong>
+                    </h1>
                 </center>
                 <br><br>
                 @foreach ($adresse as $adressedata)
@@ -38,25 +43,65 @@
                         <tbody>
                             <tr>
                                 @foreach ($client as $clientdata)
-                                    <td> {{$clientdata -> DateDeb}} </td>
-                                    <td> {{$clientdata -> NumLigue}} </td>
-                                    <td> {{$clientdata -> idFacture}} </td>
-                                    <td> {{$clientdata -> DateEcheance}} </td>
+                                <td> {{$clientdata -> DateDeb}} </td>
+                                <td> {{$clientdata -> NumLigue}} </td>
+                                <td> {{$clientdata -> idFacture}} </td>
+                                <td> {{$clientdata -> DateEcheance}} </td>
                                 @endforeach
                             </tr>
                         </tbody>
                     </table>
                 </div>
+                <br><br>
+                <div style="text-align: center">
+                    <table class="table table-borderless">
+                        <thead>
+                            <th> Réference </th>
+                            <th> Désignation </th>
+                            <th> Quantité </th>
+                            <th> PUHT </th>
+                            <th> Montant HT </th>
+                        </thead>
+                        <tbody>
+                            <?php $i = 0 ?>
+                            @foreach ($contenu as $contenudata)
+                                <tr>
+                                    <td> {{$contenudata -> ContenuFacture}} </td>
+                                    <td> {{$contenudata -> NomMat}} </td>
+                                    <td> {{$contenudata -> Qte}} </td>
+                                    <td> {{$contenudata -> Prix}}€ </td>
+                                    <td> {{$prix[$i]}}€ </td>
+                                    <?php $i++ ?>
+                                </tr>
+                            @endforeach
+                            <tr>
+                                <td colspan="4"> &ensp; </td>
+                                <td>
+                                    <h4><strong>  Total HT  </strong></h4>
+                                    <h5>{{$prix_total}}€</h5>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <br>
+                <br>
+                <h1 class="size">
+                    Déclarée à la préfecture de Meurthe et Moselle N° 3898
+                    <br> <br>
+                    Domiciliation bancaire 10278 04065 000 166911045 05
+                    <br> <br>
+                    Merci de bien vouloir préciser les références de la facture
+                    acquittée
+                    <br> <br>
+                    TVA non applicable
+                    <br> <br> <br> <br>
+                    Siret 31740105700029 <br> Tél <strong> 03.83.18.87.02 </strong>
+                    <br> Fax 03.83.18.87.03
+                </h1>
+                <br><br>
             </td>
         </tr>
     </table>
 </div>
 @endsection
-{{-- <th> Date </th>
-<th> Code Client </th>
-<th> N° Facture </th>
-<th> Échéance </th>
-<tr> {{$clientdata -> DateDeb}} </tr>
-<tr> {{$clientdata -> NumLigue}} </tr>
-<tr> {{$clientdata -> idFacture}} </tr>
-<tr> {{$clientdata -> DateEcheance}} </tr> --}}
